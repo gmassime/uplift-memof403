39dff5ca8b5facd0db5983ffa4c71b107e575d1e
giuliamassimetti <giulia.massimetti@ulb.be>
2021-03-11 20:10:56 +0100

    Added Criteo dataset + abstract dataset base class

--- datasets.py
+++ datasets.py
@@ -1,14 +1,27 @@
 import pandas as pd
+from abc import ABC, abstractmethod
 
-class HillstromDataset:
 
-    def __init__(self, file_path="http://www.minethatdata.com/Kevin_Hillstrom_MineThatData_E-MailAnalytics_DataMiningChallenge_2008.03.20.csv"):
-        """
-        Initializes a dataset using Hillstrom data
-        :param file_path: path of the .csv file with the dataset, by default it uses the URL of the web page containing the dataset
-        """
+class DatasetCSV(ABC):
+
+    def __init__(self, file_path):
         self.data = pd.read_csv(file_path)
         self.cleanDataset()
+
+    @abstractmethod
+    def cleanDataset(self):
+        pass
+
+
+class HillstromDataset(DatasetCSV):
+
+    def __init__(self,
+                 file_path="http://www.minethatdata.com/Kevin_Hillstrom_MineThatData_E-MailAnalytics_DataMiningChallenge_2008.03.20.csv"):
+        """
+        Initializes a dataset using data from the Hillstrom MineThatData E-Mail Analytics And Data Mining Challenge
+        :param file_path: path of the .csv file with the dataset, by default it uses the URL of the web page containing the dataset
+        """
+        super().__init__(file_path)
 
     def cleanDataset(self):
         """
@@ -20,7 +33,7 @@ class HillstromDataset:
         for column in ["history_segment", "zip_code", "channel"]:
             one_hot = pd.get_dummies(self.data[column])
             self.data = self.data.drop(column, axis=1).join(one_hot)
-        self.data["segment"] = self.data["segment"].replace({"No E-Mail" : 0, "Mens E-Mail" : 1, "Womens E-Mail" : 2})
+        self.data["segment"] = self.data["segment"].replace({"No E-Mail": 0, "Mens E-Mail": 1, "Womens E-Mail": 2})
         self.data = self.data.drop(["spend", "conversion"], axis=1)
 
     def getCampaignData(self, campaign=1):
@@ -36,3 +49,20 @@ class HillstromDataset:
         features = current_dataset.drop(["visit", "segment"], axis=1).to_numpy()
         treatment = current_dataset["segment"].to_numpy()
         return outcome, features, treatment
+
+
+class CriteoDataset(DatasetCSV):
+
+    def __init__(self, file_path="http://go.criteo.net/criteo-research-uplift-v2.1.csv.gz"):
+        """
+        Initializes a dataset using the Criteo Uplift Prediction data
+        :param file_path: path of the .csv file with the dataset, by default it uses the URL of the web page containing the dataset
+        """
+        super().__init__(file_path)
+
+    def cleanDataset(self):
+        """
+        Prepares the dataset for use. In particular:
+         - 'spend' and 'conversion' are dropped, we will only target 'visit'
+        """
+        self.data = self.data.drop(["conversion", "exposure"], axis=1)
