4ba2c3590ed782d9a0e3500310a771e714f1baaf
giuliamassimetti <giulia.massimetti@ulb.be>
2021-03-17 23:21:34 +0100

    getCampaignData changed to getTrainTest + added getFeatureNames

--- datasets.py
+++ datasets.py
@@ -13,7 +13,7 @@ class DatasetCSV(ABC):
         pass
 
     @abstractmethod
-    def getCampaignData(self):
+    def getTrainTest(self):
         pass
 
 
@@ -40,19 +40,33 @@ class HillstromDataset(DatasetCSV):
         self.data["segment"] = self.data["segment"].replace({"No E-Mail": 0, "Mens E-Mail": 1, "Womens E-Mail": 2})
         self.data = self.data.drop(["spend", "conversion"], axis=1)
 
-    def getCampaignData(self, campaign=1):
+    def getTrainTest(self, campaign=1, frac=0.5, seed=0):
         """
         Returns data that is compatible with CausalML meta-learners
         :param campaign: 1 if we want to analyse the mens campaign, 2 for the womens campaign
-        :return: a tuple of 3 arrays: the outcome (visit), the features and the treatment indicator (segment)
+        :return: 6 arrays, 3 for the train set and 3 for the test set: the outcome (visit), the features and the treatment indicator (segment)
         """
         if campaign != 1 and campaign != 2:
             raise ValueError
-        current_dataset = self.data[self.data["segment"] != 3 - campaign]
-        outcome = current_dataset["visit"].to_numpy()
-        features = current_dataset.drop(["visit", "segment"], axis=1).to_numpy()
-        treatment = current_dataset["segment"].to_numpy()
-        return outcome, features, treatment
+        train = self.data[self.data["segment"] != 3 - campaign].sample(frac=frac, random_state=seed).replace({campaign : 1})
+        test = self.data[self.data["segment"] != 3 - campaign].drop(train.index).sample(frac=1.0).replace({campaign : 1})   # to ensure that data is shuffled
+
+        outcome_train = train["visit"].to_numpy()
+        features_train = train.drop(["visit", "segment"], axis=1).to_numpy()
+        treatment_train = train["segment"].to_numpy()
+
+        outcome_test = test["visit"].to_numpy()
+        features_test = test.drop(["visit", "segment"], axis=1).to_numpy()
+        treatment_test = test["segment"].to_numpy()
+
+        return outcome_train, features_train, treatment_train, outcome_test, features_test, treatment_test
+
+    def getFeatureNames(self):
+        """
+        Returns the names of the features (used to visualize the uplift tree in CausalML)
+        :return: array of string
+        """
+        return list(self.data.drop(["visit", "segment"], axis=1))
 
 
 class CriteoDataset(DatasetCSV):
